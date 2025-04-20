97e9d6a974ccbce25742aae1909e26f167979359
Stantde <125204126+Stantde@users.noreply.github.com>
2025-04-20 16:49:55 +0000

    final revision

--- Chapters/4/character_grid.py
+++ Chapters/4/character_grid.py
@@ -9,18 +9,8 @@ GRID = [['.','.','.','.','.','.'],
         ['.','0','0','.','.','.'],
         ['.','.','.','.','.','.']]
 
-def print_grid(grid = [] ):
-    for i in grid:
-            print(i)
-
-#rows = len(grid)
-#columns = len(grid[0])
-print_grid(GRID)
-'''new_grid = []
-for i in range(rows):
-	new_grid[i] += i
-	for j in range(columns):
-		new_grid[i][j] += i
-		new_grid[j][i] = grid[i][j]
-for k in new_grid:
-	print(new_grid[k])'''
+for j in range(len(GRID[0])):
+	for k in range(len(GRID)):
+		print(GRID[k][j], end=' ')
+	print()
+		
